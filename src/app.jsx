5443da21404badc02252b295e12e9e3b9706f311
YY <37497289+avt1745zet@users.noreply.github.com>
2022-03-26 16:16:12 +0800

    First commit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import {Container} from '@mui/material';
+import React from 'react';
+
+const App = () => {
+  return (
+    <Container sx={{height: '100vh', display: 'flex',
+      justifyContent: 'center', alignItems: 'center'}}>
+      <h1>find-different-colors</h1>
+    </Container>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.?jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  mode: 'development',
+};
