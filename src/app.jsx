c1a74422da7a3eb4377adb7b611b8e0c922d8ff5
YY <37497289+avt1745zet@users.noreply.github.com>
2022-03-29 20:00:17 +0800

    Add timer, menu, result elements.

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,8 @@
-import {Container, createTheme,
-  CssBaseline, ThemeProvider} from '@mui/material';
+import {Box, Container, createTheme,
+  CssBaseline, ThemeProvider, Typography} from '@mui/material';
 import React from 'react';
 import MainPage from './pages/mainPage.jsx';
+import packageJson from './../package.json';
 
 const darkTheme = createTheme({
   palette: {
@@ -13,13 +14,26 @@ const App = () => {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline/>
-      <Container fixed sx={{
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-      }}>
+      <Container
+        fixed
+        sx={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
         <MainPage/>
+        <Box
+          component='footer'
+          sx={{
+            marginBlock: '0.5rem',
+          }}
+        >
+          <Typography variant='body1' align='center'>
+            Version: {packageJson.version}
+          </Typography>
+        </Box>
       </Container>
     </ThemeProvider>
   );
--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,11 +1,17 @@
-import React, {Fragment, useRef, useState} from 'react';
+import React, {Fragment, useEffect, useRef, useState} from 'react';
+import PropTypes from 'prop-types';
 import {Box} from '@mui/system';
-import {Typography} from '@mui/material';
+import {Button, Divider, Typography} from '@mui/material';
 
 const MainPage = () => {
   return (
     <Fragment>
-      <Box component='header'>
+      <Box
+        component='header'
+        sx={{
+          marginBlock: '0.5rem',
+        }}
+      >
         <Typography variant='h5' align='center'>
             Find Different Colors
         </Typography>
@@ -13,15 +19,13 @@ const MainPage = () => {
       <Box
         component='main'
         sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
           height: '100%',
         }}
       >
         <Game/>
-      </Box>
-      <Box component='footer'>
-        <Typography variant='h5' align='center'>
-            Footer
-        </Typography>
       </Box>
     </Fragment>
   );
@@ -29,58 +33,252 @@ const MainPage = () => {
 
 export default MainPage;
 
+const GameState = {
+  menu: 'menu',
+  playing: 'playing',
+  result: 'result',
+  introduce: 'introduce',
+};
+
 const Game = () => {
+  const [gameState, setGameState] = useState(GameState.menu);
+  const [currentLevel, setCurrentLevel] = useState(1);
+  let content;
+  switch (gameState) {
+    case GameState.menu:
+      content = (
+        <Menu
+          onStartButtonClick={()=>{
+            setGameState(GameState.playing);
+            setCurrentLevel(1);
+          }}
+          onHowToPlayButtonClick={()=>{
+            setGameState(GameState.introduce);
+          }}
+        />
+      );
+      break;
+    case GameState.introduce:
+      content = (
+        <Introduce onBackButtonClick={()=>{
+          setGameState(GameState.menu);
+        }}/>
+      );
+      break;
+    case GameState.playing:
+      content = (
+        <Fragment>
+          <Timer
+            time={1}
+            onTimeEnd={()=>{
+              setGameState(GameState.result);
+            }}
+          />
+          <ColorFinder
+            startLevel={1}
+            onLevelUpdate={(newLevel)=>{
+              setCurrentLevel(newLevel);
+            }}
+          />
+          <Typography variant='h5' align='center'>
+              Level: {currentLevel}
+          </Typography>
+        </Fragment>
+      );
+      break;
+    case GameState.result:
+      content = (
+        <Result
+          reachedLevel={currentLevel}
+          onGameRestartButtonClick={()=>{
+            setGameState(GameState.playing);
+            setCurrentLevel(1);
+          }}
+          onBackToMenuButtonClick={()=>{
+            setGameState(GameState.menu);
+          }}
+        />
+      );
+      break;
+  }
+  return (
+    <Fragment>
+      {content}
+    </Fragment>
+  );
+};
+
+const Menu = (props) => {
+  const {onStartButtonClick, onHowToPlayButtonClick} = props;
+  const handleStartButtonClick = () => {
+    onStartButtonClick();
+  };
+  const handleHowToPlayButtonClick = () => {
+    onHowToPlayButtonClick();
+  };
+  return (
+    <Fragment>
+      <Button
+        variant='contained'
+        color='primary'
+        onClick={handleStartButtonClick}
+      >
+        Game Start
+      </Button>
+      <Divider/>
+      <Button
+        variant='contained'
+        color='info'
+        onClick={handleHowToPlayButtonClick}
+      >
+        How to play?
+      </Button>
+    </Fragment>
+  );
+};
+
+Menu.propTypes = {
+  onStartButtonClick: PropTypes.func,
+  onHowToPlayButtonClick: PropTypes.func,
+};
+
+const Introduce = (props) => {
+  const {onBackButtonClick} = props;
+  const handleBackButtonClick = () => {
+    onBackButtonClick();
+  };
+  return (
+    <Fragment>
+      <Typography
+        align='center'
+        sx={{
+          marginBlock: '0.5rem',
+        }}
+      >
+        Reach as high a level as possible within 60 seconds,
+        when the level is higher, the number of blocks and
+        the difference in color will become larger and larger.
+        <br/>
+        <br/>
+        Keep your eyes clear enough, good luck :)
+      </Typography>
+      <Button variant='contained' onClick={handleBackButtonClick}>
+        Back to menu
+      </Button>
+    </Fragment>
+  );
+};
+
+Introduce.propTypes = {
+  onBackButtonClick: PropTypes.func,
+};
+
+const Timer = (props) => {
+  const {time, onTimeEnd} = props;
+  const [remainTime, setRemainTime] = useState(time);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      countDown();
+    }, 1000);
+  }, [remainTime]);
+
+  const countDown = () => {
+    if (remainTime > 0) {
+      setRemainTime(remainTime - 1);
+    } else {
+      onTimeEnd();
+    }
+  };
+
+  return (
+    <Typography variant='h5' align='center'>
+      Time: {remainTime}
+    </Typography>
+  );
+};
+
+Timer.propTypes = {
+  time: PropTypes.number.isRequired,
+  onTimeEnd: PropTypes.func.isRequired,
+};
+
+const ColorFinder = (props) => {
+  const {startLevel, onLevelUpdate} = props;
+
+  const [level, setLevel] = useState(startLevel);
+
+  const getSize = () => {
+    return Math.floor((level - 1) / 5) + 2;
+  };
+  const size = getSize();
+  const getRandomPosition = () => {
+    return [
+      Math.floor(Math.random() * size),
+      Math.floor(Math.random() * size),
+    ];
+  };
+  const [answerPosition, setAnswerPosition] = useState(getRandomPosition());
+
   const gameFrameRef = useRef(null);
   const squareRef = useRef(null);
 
-  window.addEventListener('resize', ()=>{
+  useEffect(()=>{
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleResize = () => {
     const size = Math.min(
         gameFrameRef.current.offsetWidth,
         gameFrameRef.current.offsetHeight );
     squareRef.current.style.width = size + 'px';
-    squareRef.current.style.height = size+ 'px';
-  });
-  window.addEventListener('load', ()=>{
-    const size = Math.min(
-        gameFrameRef.current.offsetWidth,
-        gameFrameRef.current.offsetHeight );
-    squareRef.current.style.width = size + 'px';
-    squareRef.current.style.height = size+ 'px';
-  });
-
-  const [level, setLevel] = useState(1);
-
-  const rows = [];
-
-  const questionColor = {
-    r: Math.random() * 255,
-    g: Math.random() * 255,
-    b: Math.random() * 255,
+    squareRef.current.style.height = size + 'px';
   };
 
-  const answerColor = {
-    r: questionColor.r + Math.random() * 255 / level,
-    g: questionColor.g + Math.random() * 255 / level,
-    b: questionColor.b + Math.random() * 255 / level,
+  const getRandomColor = () => {
+    return {
+      r: Math.random() * 255,
+      g: Math.random() * 255,
+      b: Math.random() * 255,
+    };
   };
 
-  const size = [level + 1, level + 1];
+  const questionColor = getRandomColor();
 
-  const answerPosition = [
-    Math.floor(Math.random()*size[0]),
-    Math.floor(Math.random()*size[1]),
-  ];
+  const generateAnswerColor = () => {
+    const getDiffrence = () => {
+      const randomSign = Math.random() - 0.5 < 0? -1: 1;
+      const diffrence = randomSign * (Math.random() * 255 / (level / 2));
+      return diffrence;
+    };
+    return {
+      r: questionColor.r + getDiffrence(),
+      g: questionColor.g + getDiffrence(),
+      b: questionColor.b + getDiffrence(),
+    };
+  };
+
+  const answerColor = generateAnswerColor();
 
   const handleBlockClick = ( position ) => {
     if (position[0] === answerPosition[0] &&
         position[1] === answerPosition[1]) {
-      setLevel(level+1);
+      const newLevel = level + 1;
+      setLevel(newLevel);
+      onLevelUpdate(newLevel);
+      setAnswerPosition(getRandomPosition());
     }
   };
 
-  for ( let i = 0; i < size[0]; i++) {
+  const rows = [];
+
+  for ( let i = 0; i < size; i++) {
     const clomns = [];
-    for ( let j = 0; j < size[1]; j++) {
+    for ( let j = 0; j < size; j++) {
       const color = i === answerPosition[0] && j === answerPosition[1]?
       answerColor: questionColor;
       const block = (
@@ -89,7 +287,7 @@ const Game = () => {
           key={ j.toString()}
           sx={{
             height: '100%',
-            width: `${100/(level + 1)}%`,
+            width: `${100/size}%`,
             backgroundColor: `rgb(
             ${color.r},
             ${color.g},
@@ -104,8 +302,8 @@ const Game = () => {
     }
     rows.push(
         <Box
-          key={ i.toString()}
-          sx={{height: `${100/(level + 1)}%`}}
+          key={i.toString()}
+          sx={{height: `${100/size}%`}}
         >
           {clomns}
         </Box>,
@@ -127,4 +325,57 @@ const Game = () => {
       </Box>
     </Box>
   );
+};
+
+ColorFinder.propTypes = {
+  startLevel: PropTypes.number.isRequired,
+  onLevelUpdate: PropTypes.func,
+};
+
+const Result = (props) => {
+  const {reachedLevel,
+    onGameRestartButtonClick, onBackToMenuButtonClick} = props;
+  const handleGameRestartButtonClick = () => {
+    onGameRestartButtonClick();
+  };
+  const handleBackToMenuButtonClick = () => {
+    onBackToMenuButtonClick();
+  };
+  return (
+    <Fragment>
+      <Typography variant='h2' align='center'>
+        WOW!
+      </Typography>
+      <Typography
+        variant='h5'
+        align='center'
+        sx={{
+          marginBlock: '0.5rem',
+        }}
+      >
+        You reached level: {reachedLevel}
+      </Typography>
+      <Button
+        variant='contained'
+        color='primary'
+        onClick={handleGameRestartButtonClick}
+      >
+        Game Restart
+      </Button>
+      <Divider/>
+      <Button
+        variant='contained'
+        color='secondary'
+        onClick={handleBackToMenuButtonClick}
+      >
+        Back To Menu
+      </Button>
+    </Fragment>
+  );
+};
+
+Result.propTypes = {
+  reachedLevel: PropTypes.number.isRequired,
+  onGameRestartButtonClick: PropTypes.func,
+  onBackToMenuButtonClick: PropTypes.func,
 };
